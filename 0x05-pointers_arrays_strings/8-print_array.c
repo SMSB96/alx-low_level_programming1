a90ea791ca6027c981db9b0806176f70ea5fdfe9
SARA MIAD <saramiad96@example.com>
2023-06-27 17:48:26 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,4 @@
 #include "main.h"
-
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int array[5];
-
-array[0] = 98;
-array[1] = 402;
-array[2] = -198;
-array[3] = 298;
-array[4] = -1024;
-print_array(array, 5);
-return (0);
-}
-root@edb2e835798f:/alx-low_level_programming/0x05-pointers_arrays_strings# cat 8-print_array.c
-#include "main.h"
 #include <stdio.h>
 
 /**
