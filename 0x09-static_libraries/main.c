fdb1283e14ff4eee247d841dd67b3309830f5fc0
SARA MIAD <saramiad96@example.com>
2023-07-12 06:31:10 -0700

    task0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/* 
+ * Main function
+ * Entry point of the program
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, \
+my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
